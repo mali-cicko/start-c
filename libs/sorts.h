e13b2e59e8fd520f6ce39186af2fe22e05441660
Stefan Isidorovic <Stefan.Isidorovic@seavus.com>
2017-10-25 14:55:37 +0200

    Insertion and selection sort implemented.

--- libs/sorts.h
+++ libs/sorts.h
@@ -1,7 +1,9 @@
 #ifndef __SORTS__H__
 #define __SORTS__H__
 
-
+// Bubble sort implementation
+// @param arr - array to be sorted
+// @param n   - size of array to be sorted
 void bubble_sort(int* arr, int n){
   int i, j, tmp;
   for (i = n; i >= 0; i--){
@@ -13,6 +15,39 @@ void bubble_sort(int* arr, int n){
         }
     }
   }
+}
+
+void selection_sort(int* arr, int n) {
+  int i, j, tmp, min = 0;
+  for (i = 0; i < n; i++){
+    for (j = i; j < n; j++){
+	if (arr[j] < arr[min]){
+	  min = j;
+	}
+    }
+    tmp = arr[i];
+    arr[i] = arr[min];
+    arr[min] = tmp;
+      
+  }
+      
+}
+
+
+void insertion_sort(int* arr, int n) {
+    int i, j, key;
+    for (j = 1; j < n; j++){
+      key = arr[j];
+      i = j - 1;
+      
+      while((i >= 0) && (arr[i] > key)){
+	arr[i + 1] = arr[i];
+	i = i - 1;
+      }
+      arr[i + 1] = key;
+      
+      
+    }
 }
 
 
--- main.c
+++ main.c
@@ -5,32 +5,26 @@
 
 int main(int argc, char** argv){
   
-  int n = 15;
-  int* arr = i_random_arr(15);
+  int n = 50000;
+  int* arr = i_random_arr_b(n, -10000, 10000);
+  int* arr1 = copy_arr(arr, n);
+  int* arr2 = copy_arr(arr, n);
   
   start();
   bubble_sort(arr, n);
-  printf("%d Time %lld ms\n", n, time_elapsed_ms());
-  
-  free(arr);
-  n = 5000;
-  arr = i_random_arr(n);
+  printf("%d Time for bubble sort %lld ms\n", n, time_elapsed_ms());
   
   start();
-  bubble_sort(arr, n);
-  printf("%d Time %lld ms\n", n, time_elapsed_ms());
-  
-  free(arr);
-  n = 500000;
-  arr = i_random_arr(n);
+  selection_sort(arr1, n);
+  printf("%d Time for selection sort %lld ms\n", n, time_elapsed_ms());
   
   start();
-  bubble_sort(arr, n);
-  printf("%d Time %lld ms\n", n, time_elapsed_ms());
-    
-  
-  
-  
+  insertion_sort(arr2, n);
+  printf("%d Time for insertion sort %lld ms\n", n, time_elapsed_ms());
+  if (is_sorted(arr2, n) == 0)
+    printf("Ovaj niz je sortiran\n");
+  else
+     printf("Nesto ne valja\n") ;
   
   return 0;
 }
