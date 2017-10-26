4206267e1edc0a8b57894279811d384cae1a8ebe
Katarina Dimitrijevic <katjaa06@gmail.com>
2017-10-26 23:12:55 +0200

    Fixed sorting algorithms to sort everything. std qsort api used.

--- libs/sorts.h
+++ libs/sorts.h
@@ -13,9 +13,10 @@
 #include<stdlib.h>
 #include<string.h>
 
-void swap(void* first, void* second, unsigned size){
+void swap(void* first, void* second, int size){
   char* first_1 = (char*)first;
   char* second_1 = (char*)second;
+
   char* tmp = (char*) malloc(size);
 
   memcpy(tmp, first_1, size);
@@ -23,53 +24,51 @@ void swap(void* first, void* second, unsigned size){
   memcpy(second_1, tmp, size);
 
   free(tmp);
-
  }
-
-
 
 // Bubble sort implementation
 // @param arr - array to be sorted
 // @param n   - size of array to be sorted
-void bubble_sort(int* arr, int n){
-  int i, j, tmp;
+void bubble_sort(void* arr, int n, int size, int (*cmp)(const void*, const void*)){
+  int i, j;
 
   for (i = n; i >= 0; i--){
     for (j = 0; j < i; j++){
-	if (arr[j] > arr[j+1]){
-	    swap(&arr[j], &arr[j+1], sizeof(int));
+	if (cmp(arr + size*j, arr + size*(j + 1)) == 1){
+	    swap(arr + size*j, arr+size*(j + 1), size);
         }
     }
   }
 }
 
 
-void selection_sort(int* arr, int n) {
-  int i, j, tmp, min = 0;
+void selection_sort(void* arr, int n, int size, int (*cmp)(const void*, const void*)) {
+  int i, j, min = 0;
   for (i = 0; i < n; i++){
     for (j = i; j < n; j++){
-	if (arr[j] < arr[min]){
+	if (cmp(arr + j * size, arr + min * size) == 1){
 	  min = j;
 	}
     }
-    swap(&arr[i], &arr[min], sizeof(int));
+    swap(arr + i*size, arr + min*size, size);
 
   }
 
 }
 
 
-void insertion_sort(int* arr, int n) {
-    int i, j, key;
+void insertion_sort(void* arr, int n, int size, int (*cmp)(const void*, const void*)) {
+    int i, j;
+    char* key = (char*) malloc(size);
     for (j = 1; j < n; j++){
-      key = arr[j];
+      memcpy(key, arr + j*size, size);
       i = j - 1;
 
-      while((i >= 0) && (arr[i] > key)){
-	arr[i + 1] = arr[i];
+      while((i >= 0) && (cmp(key, arr + i*size) == 1)){
+	memcpy(arr + size * (i + 1), arr + size * i, size);
 	i = i - 1;
       }
-      arr[i + 1] = key;
+      memcpy(arr + size * (i + 1), key, size);
     }
 }
 
--- main.c
+++ main.c
@@ -1,30 +1,35 @@
 #include <stdio.h>
-#include "libs/arr_util.h" 
+#include "libs/arr_util.h"
 #include "libs/sorts.h"
 #include "libs/stopwatch.h"
 
+int cmp(const void* a, const void* b){
+  return *(int*)a < *(int*)b;
+}
+
 int main(int argc, char** argv){
-  
+
   int n = 50000;
   int* arr = i_random_arr_b(n, -10000, 10000);
   int* arr1 = copy_arr(arr, n);
   int* arr2 = copy_arr(arr, n);
-  
+  int* arr3 = copy_arr(arr, n);
+
   start();
-  bubble_sort(arr, n);
+  bubble_sort(arr, n, sizeof(int), cmp);
   printf("%d Time for bubble sort %lld ms\n", n, time_elapsed_ms());
-  
+
   start();
-  selection_sort(arr1, n);
+  selection_sort(arr1, n, sizeof(int), cmp);
   printf("%d Time for selection sort %lld ms\n", n, time_elapsed_ms());
-  
+
   start();
-  insertion_sort(arr2, n);
+  insertion_sort(arr2, n, sizeof(int), cmp);
   printf("%d Time for insertion sort %lld ms\n", n, time_elapsed_ms());
-  if (is_sorted(arr2, n) == 0)
-    printf("Ovaj niz je sortiran\n");
-  else
-     printf("Nesto ne valja\n") ;
-  
+
+  start();
+  qsort(arr3, n, sizeof(int), cmp);
+  printf("%d Time for qsort %lld ms\n", n, time_elapsed_ms());
+
   return 0;
 }
